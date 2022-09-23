b73511056b68a278ea4098e3eb06c9b7d9bec83a
Igor Novikov <igor.novikov@umbrellait.com>
2022-09-23 14:11:27 +0300

    nav component has been connected

--- app/src/main/java/com/umbrella/flickr/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/umbrella/flickr/presentation/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.umbrella.flickr.presentation.login
 
 import androidx.fragment.app.Fragment
+import com.umbrella.flickr.R
 
-class LoginFragment : Fragment()
+class LoginFragment : Fragment(R.layout.fragment_login)
